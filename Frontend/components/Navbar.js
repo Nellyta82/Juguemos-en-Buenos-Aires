fe3bb80a27f38b4a186e3987f12fd02a3f1c04e4
repoNelly <nellytachirinos@gmail.com>
2023-10-31 15:15:30 -0300

    nav-bar

--- Frontend/components/Navbar.js
+++ Frontend/components/Navbar.js
@@ -1,1 +1,14 @@
-hola
+const toggleButton = document.getElementById('menu-hamburguesa')
+const navWrapper = document.getElementById('nav')
+
+toggleButton.addEventListener('click',() => {
+  toggleButton.classList.toggle('close')
+  navWrapper.classList.toggle('show')
+})
+
+navWrapper.addEventListener('click',e => {
+  if(e.target.id === 'nav'){
+    navWrapper.classList.remove('show')
+    toggleButton.classList.remove('close')
+  }
+})
